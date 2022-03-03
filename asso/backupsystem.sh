6174d8ee5ec125559465e5413c9b2246fd0ce263
Chmielewski Filip <filipch12@gmail.com>
2022-03-03 14:22:50 +0100

    q

--- asso/backupsystem.sh
+++ asso/backupsystem.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
 
-DATE=$(date +%m:%d:%y)
+FULLDATE=$(date +%c)
 
-tar -czvf /home/keped/backup/backupfolder/$DATE.tar.gz /home/keped/backup/backup_sorcefolder
+FILENAME=$()
 
-echo  "second backup" > /var/log/backup.log
+A=$(date +%N -r /home/keped/backup/backupfolder) 
+
+L=$(date +%N -r /home/keped/backup/backup_sorcefolder)
+
+INITIALSIZE=$(wc -c /home/keped/backup/backup_sorcefolder/experimentalfile | awk '{print $1}')
+
+ARCHIVEDFILESIZE=$(tar ztvf $FILENAME.tar.gz -v --wildcards '*FOOBAR*')
+
+if [ $L -gt $A ] ; then	
+
+tar -czvf /home/keped/backup/backupfolder/$FILENAME.tar.gz /home/keped/backup/backup_sorcefolder
+
+echo -e "most recent archivisation of $FILENAME at: $FULLDATE \n filesize cut down from $INITIALFILESIZE bites to > $ARCHIVEDFILESIZE bites" >> /var/log/backup.log
+
+else
+
+echo "nothing is to be done"
+
+fi
 
