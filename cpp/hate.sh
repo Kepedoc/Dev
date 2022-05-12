783ceb7479ba6451f7519774c65cb41b5e280417
Chmielewski Filip <filipch12@gmail.com>
2022-05-12 12:00:05 +0200

    i love this code

--- cpp/hate.sh
+++ cpp/hate.sh
@@ -2,8 +2,15 @@
 
 number=1
 
-for i in {1..100}
+for i in {1..500}
 do
+	if [ $i = 69 ]
+	then
 	echo "i hate $number niggers :D"
+	fi
+	if [ $i = 420 ]
+	then
+	echo "i hate $number niggers :D"
+	fi
 	let "number++"
 done
