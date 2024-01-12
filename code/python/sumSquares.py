2eb224fa3e57937eef5a931e4e31a0fa656c6f73
Kepedoc <filipch12@gmail.com>
2024-01-12 21:26:12 +0100

    finnaly

--- code/python/sumSquares.py
+++ code/python/sumSquares.py
@@ -1,11 +1,10 @@
 list = []
 sum = 0
-print("type 5 random numbers!\n")
+print("type 5 random numbers!")
 for x in range(5):
     x = int(input())
     if x % 2 == 0:
         x = pow(x,2)
         list.append(x)
-for x in list:
-    sum += x
+        sum += x
 print(sum)
