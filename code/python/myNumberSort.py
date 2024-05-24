51719050939ab02d0a443338be21e30ce4e56f96
Filip <90800617+Kepedoc@users.noreply.github.com>
2024-05-24 03:02:31 +0200

    Update myNumberSort.py

--- code/python/myNumberSort.py
+++ code/python/myNumberSort.py
@@ -1,5 +1,12 @@
-def sort(x):
-    pass
-
-list = [1 , 0 , 2 , 3.5 , 20 , 230 , -112 , 0.1]
-print(list)
+length = int(input("input how many numbers you want to add"))
+tab = []
+for x in range(length):
+    number = int(input())
+    tab.append(number)
+sort = int(input("sort ascending = 1\n sort descending = 0\n"))
+if sort == 1:
+    tab.sort()
+else:
+    tab.sort()
+    tab.reverse()
+print(tab)
