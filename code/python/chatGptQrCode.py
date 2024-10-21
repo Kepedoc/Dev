1cd27a8661f51accbf2d0d17742047c53f594eec
Kepedoc <filipch12@gmail.com>
2024-10-21 10:58:49 +0200

    u

---
+++ code/python/chatGptQrCode.py
@@ -0,0 +1,22 @@
+import qrcode
+
+# Get user input
+data = input("Enter the data you want to encode in the QR code: ")
+
+# Generate QR code
+qr = qrcode.QRCode(
+    version=1,
+    error_correction=qrcode.constants.ERROR_CORRECT_L,
+    box_size=10,
+    border=4,
+)
+qr.add_data(data)
+qr.make(fit=True)
+
+# Create an image from the QR Code instance
+img = qr.make_image(fill_color="black", back_color="white")
+
+# Save the image
+img.save("qrcode.png")
+
+print("QR code generated and saved as 'qrcode.png'.")
